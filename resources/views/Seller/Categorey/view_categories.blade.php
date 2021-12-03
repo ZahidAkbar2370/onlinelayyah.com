a8e4a77a6e268c42d54e2bab42f4cb992d934c3f
ZahidAkbar2370 <zahidjakhar2370@gmail.com>
2021-12-04 01:21:16 +0500

    zahid add search into categorey

--- resources/views/Seller/Categorey/view_categories.blade.php
+++ resources/views/Seller/Categorey/view_categories.blade.php
@@ -3,14 +3,19 @@
 
 <div class="row">
 	<h3 class="text-center mb-3">All Categories</h3>
+			<a href="{{url('seller-add-categorey')}}" style="width: 190px"><button class="btn btn-primary" style="width: 190px">Add Categorey</button></a>
 		<div class="col-12 col-md-12 col-lg-12 col-xxl-12 d-flex">
 			<div class="card flex-fill">
 
 				<div class="card-header">
-					<a href="{{url('seller-add-categorey')}}"><button class="btn btn-primary">Add Categorey</button></a>
+					<select id="selectInput" style="width: 180px;height: 30px">
+						<option value="1">Name</option>
+						<option value="2">Publication Status</option>
+					</select>
+						<input type="text" name="myInput" id="myInput" onkeyup="myFunction()"  style="width: 180px;height: 30px">
 				</div>
 				
-				<table class="table table-hover my-0">
+				<table class="table table-hover my-0"  id="myTable">
 					<thead>
 						<tr>
 							<th>Sr #</th>
