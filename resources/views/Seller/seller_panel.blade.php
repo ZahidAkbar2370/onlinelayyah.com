290598ec5d0c25ac5a698b5939af3adf1ab3c316
ZahidAkbar2370 <zahidjakhar2370@gmail.com>
2021-12-04 22:57:55 +0500

    seller design seting

--- resources/views/Seller/seller_panel.blade.php
+++ resources/views/Seller/seller_panel.blade.php
@@ -104,7 +104,7 @@
                     </li>
 
                     <li class="sidebar-item">
-                        <a class="sidebar-link" href="#">
+                        <a class="sidebar-link" href="{{url('seller-add-complain')}}">
                             <i class="align-middle" data-feather="message-square"></i> <span class="align-middle">Complain</span>
                          </a>
                     </li>
