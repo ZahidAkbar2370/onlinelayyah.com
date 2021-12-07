49b3d00f900757837624d172fad09f592247a61d
ZahidAkbar2370 <zahidjakhar2370@gmail.com>
2021-12-08 00:48:13 +0500

    seller complete except sale

--- resources/views/App/Page/home.blade.php
+++ resources/views/App/Page/home.blade.php
@@ -3,119 +3,7 @@
 
 
   <!-- Header Inner -->
-    <div class="header-inner">
-      <div class="container">
-        <div class="cat-nav-head">
-          <div class="row">
-
-            <div class="col-lg-3">
-              <div class="all-category">
-                <h3 class="cat-heading"><i class="fa fa-bars" aria-hidden="true"></i>CATEGORIES</h3>
-                <ul class="main-category">
-                  <li><a href="#">New Arrivals <i class="fa fa-angle-right" aria-hidden="true"></i></a>
-                    <ul class="sub-category">
-                      <li><a href="#">accessories</a></li>
-                      <li><a href="#">best selling</a></li>
-                      <li><a href="#">top 100 offer</a></li>
-                      <li><a href="#">sunglass</a></li>
-                      <li><a href="#">watch</a></li>
-                      <li><a href="#">man’s product</a></li>
-                      <li><a href="#">ladies</a></li>
-                      <li><a href="#">westrn dress</a></li>
-                      <li><a href="#">denim </a></li>
-                    </ul>
-                  </li>
-                  <li class="main-mega"><a href="#">best selling <i class="fa fa-angle-right" aria-hidden="true"></i></a>
-                    <ul class="mega-menu">
-                      <li class="single-menu">
-                        <a href="#" class="title-link">Shop Kid's</a>
-                        <div class="image">
-                          <img src="https://via.placeholder.com/225x155" alt="#">
-                        </div>
-                        <div class="inner-link">
-                          <a href="#">Kids Toys</a>
-                          <a href="#">Kids Travel Car</a>
-                          <a href="#">Kids Color Shape</a>
-                          <a href="#">Kids Tent</a>
-                        </div>
-                      </li>
-                      <li class="single-menu">
-                        <a href="#" class="title-link">Shop Men's</a>
-                        <div class="image">
-                          <img src="https://via.placeholder.com/225x155" alt="#">
-                        </div>
-                        <div class="inner-link">
-                          <a href="#">Watch</a>
-                          <a href="#">T-shirt</a>
-                          <a href="#">Hoodies</a>
-                          <a href="#">Formal Pant</a>
-                        </div>
-                      </li>
-                      <li class="single-menu">
-                        <a href="#" class="title-link">Shop Women's</a>
-                        <div class="image">
-                          <img src="https://via.placeholder.com/225x155" alt="#">
-                        </div>
-                        <div class="inner-link">
-                          <a href="#">Ladies Shirt</a>
-                          <a href="#">Ladies Frog</a>
-                          <a href="#">Ladies Sun Glass</a>
-                          <a href="#">Ladies Watch</a>
-                        </div>
-                      </li>
-                    </ul>
-                  </li>
-                  <li><a href="#">accessories</a></li>
-                  <li><a href="#">top 100 offer</a></li>
-                  <li><a href="#">sunglass</a></li>
-                  <li><a href="#">watch</a></li>
-                  <li><a href="#">man’s product</a></li>
-                  <li><a href="#">ladies</a></li>
-                  <li><a href="#">westrn dress</a></li>
-                  <li><a href="#">denim </a></li>
-                </ul>
-              </div>
-            </div>
-
-            <div class="col-lg-9 col-12">
-              <div class="menu-area">
-                <!-- Main Menu -->
-                <nav class="navbar navbar-expand-lg">
-                  <div class="navbar-collapse"> 
-                    <div class="nav-inner"> 
-                      <ul class="nav main-menu menu navbar-nav">
-                          <li class="active"><a href="{{url('index')}}">Home</a></li>
-                          <li><a href="{{url('products')}}">Product</a></li>                        
-                          <li><a href="{{url('shops')}}">Shop<i class="ti-angle-down"></i></a>
-                            <!-- <span class="new"></span> -->
-                          <!--   <ul class="dropdown">
-                              <li><a href="#">Cart</a></li>
-                              <li><a href="#">Checkout</a></li>
-                            </ul> -->
-                          </li>
-                          <!-- <li><a href="#">Pricing</a></li> -->
-                          <!-- <li><a href="#">Pages</a></li>                   -->
-                          <!-- <li><a href="#">Blog<i class="ti-angle-down"></i></a>
-                            <ul class="dropdown">
-                              <li><a href="#">Blog Single Sidebar</a></li>
-                            </ul>
-                          </li> -->
-                          <li><a href="{{url('contact')}}">Contact Us</a></li>
-                        </ul>
-                    </div>
-                  </div>
-                </nav>
-                <!--/ End Main Menu --> 
-              </div>
-            </div>
-
-
-          </div>
-        </div>
-      </div>
-    </div>
-    <!--/ End Header Inner -->
-  </header>
+    
   <!--/ End Header -->
   
   <!-- Slider Area -->
@@ -435,47 +323,66 @@
       <div class="row">
         <div class="col-12">
           <div class="section-title">
-            <h2>From Our Blog</h2>
+            <h2>Our Team</h2>
           </div>
         </div>
       </div>
       <div class="row">
-        <div class="col-lg-4 col-md-6 col-12">
+        <div class="col-lg-3 col-md-6 col-12">
           <!-- Start Single Blog  -->
           <div class="shop-single-blog">
-            <img src="{{asset('app/images/profileImage/zahid.jpg')}}"  class="rounded-circle" style="width: 150px;height: 150px"alt="#">
+            <img src="{{asset('app/images/blogImage/arslan.jpg')}}"  class="rounded-circle" style="width: 150px;height: 150px"alt="#">
             <div class="content">
-              <p class="date">03 Dec , 2020. Monday</p>
+              <p class="date">Frontend Developer</p>
+              <a href="#" class="title">Arslan Ahmad</a>
+              <!-- <a href="#" class="more-btn">Continue Reading</a> -->
+            </div>
+          </div>
+          <!-- End Single Blog  -->
+        </div>
+        <div class="col-lg-3 col-md-6 col-12">
+          <!-- Start Single Blog  -->
+          <div class="shop-single-blog">
+            <img src="{{asset('app/images/blogImage/zahid.jpg')}}" class="rounded-circle" style="width: 150px;height: 150px"  alt="#">
+            <div class="content">
+              <p class="date">Full Stack Developer</p>
               <a href="#" class="title">Zahid Akbar</a>
-              <a href="#" class="more-btn">Continue Reading</a>
+              <!-- <a href="#" class="more-btn">Continue Reading</a> -->
             </div>
           </div>
           <!-- End Single Blog  -->
         </div>
-        <div class="col-lg-4 col-md-6 col-12">
+
+        <div class="col-lg-3 col-md-6 col-12">
           <!-- Start Single Blog  -->
           <div class="shop-single-blog">
-            <img src="{{asset('app/images/profileImage/zahid.jpg')}}" class="rounded-circle" style="width: 150px;height: 150px"  alt="#">
+            <img src="{{asset('app/images/blogImage/kamran.jpg')}}" class="rounded-circle" style="width: 150px;height: 150px"alt="#">
             <div class="content">
-              <p class="date">22 July, 2020. Monday</p>
-              <a href="#" class="title">Jakhar</a>
-              <a href="#" class="more-btn">Continue Reading</a>
+              <p class="date">Software Engineer</p>
+              <a href="#" class="title">Kamran Kalasra</a>
+              <!-- <a href="#" class="more-btn">Continue Reading</a> -->
             </div>
           </div>
           <!-- End Single Blog  -->
         </div>
-        <div class="col-lg-4 col-md-6 col-12">
+
+
+        <div class="col-lg-3 col-md-6 col-12">
           <!-- Start Single Blog  -->
           <div class="shop-single-blog">
-            <img src="{{asset('app/images/profileImage/zahid.jpg')}}" class="rounded-circle" style="width: 150px;height: 150px"alt="#">
+            <img src="{{asset('app/images/blogImage/rida.jpg')}}" class="rounded-circle" style="width: 150px;height: 150px"alt="#">
             <div class="content">
-              <p class="date">22 July, 2020. Monday</p>
-              <a href="#" class="title">Zahid</a>
-              <a href="#" class="more-btn">Continue Reading</a>
+              <p class="date">Backend Developer</p>
+              <a href="#" class="title">Rida Abbas</a>
+              <!-- <a href="#" class="more-btn">Continue Reading</a> -->
             </div>
           </div>
           <!-- End Single Blog  -->
         </div>
+
+        
+
+
       </div>
     </div>
   </section>
--- resources/views/App/app_layout.blade.php
+++ resources/views/App/app_layout.blade.php
@@ -90,19 +90,22 @@
         </div>
       </div>
     </div>
+
+
     <!-- End Topbar -->
     <div class="middle-inner">
       <div class="container">
         <div class="row">
-          <div class="col-lg-2 col-md-2 col-12">
+          <div class="col-lg-3 col-md-2 col-12">
             <!-- Logo -->
             <div class="logo">
-              <a href="{{url('index')}}"><img src="app/images/logo.png" alt="logo"></a>
+              <!-- <a href="{{url('index')}}"><img src="app/images/logo.png" alt="logo"></a> -->
+              <a href="{{url('index')}}"><h4>Online Layyah</h4></a>
             </div>
             <!--/ End Logo -->
             <!-- Search Form -->
             <div class="search-top">
-              <div class="top-search"><a href="#0"><i class="ti-search"></i></a></div>
+              <div class="top-search"><a href="#"><i class="ti-search"></i></a></div>
               <!-- Search Form -->
               <div class="search-top">
                 <form class="search-form">
@@ -115,7 +118,7 @@
             <!--/ End Search Form -->
             <div class="mobile-nav"></div>
           </div>
-          <div class="col-lg-8 col-md-7 col-12">
+          <div class="col-lg-7 col-md-7 col-12">
             <div class="search-bar-top">
               <div class="search-bar">
                 <select>
@@ -151,6 +154,59 @@
         </div>
       </div>
     </div>
+
+    <div class="header-inner">
+      <div class="container">
+        <div class="cat-nav-head">
+          <div class="row">
+
+            <div class="col-lg-3">
+              <div class="all-category">
+                <h3 class="cat-heading"><i class="fa fa-bars" aria-hidden="true"></i>CATEGORIES</h3>
+                
+              </div>
+            </div>
+
+            <div class="col-lg-9 col-12">
+              <div class="menu-area">
+                <!-- Main Menu -->
+                <nav class="navbar navbar-expand-lg">
+                  <div class="navbar-collapse"> 
+                    <div class="nav-inner"> 
+                      <ul class="nav main-menu menu navbar-nav">
+                          <li class="active"><a href="{{url('index')}}">Home</a></li>
+                          <li><a href="{{url('products')}}">Products</a></li>                        
+                          <li><a href="{{url('shops')}}">Shops </a>
+                            <!-- <i class="ti-angle-down"></i> -->
+                            <!-- <span class="new"></span> -->
+                          <!--   <ul class="dropdown">
+                              <li><a href="#">Cart</a></li>
+                              <li><a href="#">Checkout</a></li>
+                            </ul> -->
+                          </li>
+                          <!-- <li><a href="#">Pricing</a></li> -->
+                          <!-- <li><a href="#">Pages</a></li>                   -->
+                          <!-- <li><a href="#">Blog<i class="ti-angle-down"></i></a>
+                            <ul class="dropdown">
+                              <li><a href="#">Blog Single Sidebar</a></li>
+                            </ul>
+                          </li> -->
+                          <li><a href="{{url('contact')}}">Contact Us</a></li>
+                        </ul>
+                    </div>
+                  </div>
+                </nav>
+                <!--/ End Main Menu --> 
+              </div>
+            </div>
+
+
+          </div>
+        </div>
+      </div>
+    </div>
+    <!--/ End Header Inner -->
+  </header>
   
   
 @yield("content")
@@ -189,7 +245,8 @@
             <!-- Single Widget -->
             <div class="single-footer about">
               <div class="logo">
-                <a href="index.html"><img src="app/images/logo2.png" alt="#"></a>
+                <!-- <a href="{{url('index')}}"><img src="app/images/logo2.png" alt="#"></a> -->
+                <h1 style="color: white">Online Layyah</h1>
               </div>
               <p class="text">Praesent dapibus, neque id cursus ucibus, tortor neque egestas augue,  magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus.</p>
               <p class="call">Got Question? Call us 24/7<span><a href="tel:03081312527">+92 308 1312527</a></span></p>
@@ -201,10 +258,11 @@
             <div class="single-footer links">
               <h4>Information</h4>
               <ul>
-                <li><a href="#">About Us</a></li>
-                <li><a href="#">Faq</a></li>
+                <li><a href="{{url('index')}}">Home</a></li>
+                <li><a href="{{url('products')}}">Products</a></li>
+                <li><a href="{{url('shops')}}">Shops</a></li>
                 <li><a href="#">Terms & Conditions</a></li>
-                <li><a href="#">Contact Us</a></li>
+                <li><a href="{{url('contact')}}">Contact Us</a></li>
                 <li><a href="#">Help</a></li>
               </ul>
             </div>
@@ -262,7 +320,7 @@
             </div>
             <div class="col-lg-6 col-12">
               <div class="right">
-                <img src="images/payments.png" alt="#">
+                <img src="app/images/payments.png" alt="#">
               </div>
             </div>
           </div>
